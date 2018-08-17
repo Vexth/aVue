5258dc9c2101033993f8c508746f414dd5497a29
2fx0one <2fx0one@gmail.com>
2018-08-17 12:56:46 +0800

    update

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,4 +2,6 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   BASE_API: '"http://192.168.10.170:8080/"' //王昆本地
+  // BASE_API: '"http://192.168.10.133:8080/"' //宇哥本地
+  // BASE_API: '"http://192.168.10.153:8080/"' //彭毅本地
 }
