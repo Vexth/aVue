83f4a5b57a1862bd244215fb26a7f973c931895e
2fx0one <2fx0one@gmail.com>
2018-08-21 16:10:49 +0800

    update

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ module.exports = {
   // BASE_API: '"https://shopwk.jxxykj.cn/"' //王昆线上
   // BASE_API: '"http://192.168.10.170:8080/"' //王昆本地
   BASE_API: '"http://192.168.10.133:8080/"' //余哥本地
-  // BASE_API: '"http://192.168.10.170:8080/"' //王昆本地
   // BASE_API: '"http://192.168.10.153:8080/"' //彭毅本地
+  // BASE_API: '"https://shoppengyi.jxxykj.cn/"' //彭毅线上
 }
