477b4b567724e339b93e25a9141095a7e27e03b6
2fx0one <2fx0one@gmail.com>
2018-08-21 16:03:09 +0800

    update

--- src/permission.js
+++ src/permission.js
@@ -32,10 +32,21 @@ router.beforeEach((to, from, next) => {
       next({ path: '/' })
       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
-      // console.log(to.path)
-      store.getters.addRouters.forEach(route => {
-        console.log(route)
-      })
+      // console.log('to.path = ' + to.path)
+      // const pathList = []
+      // store.getters.addRouters.forEach(route => {
+      //   // console.log(route)
+      //   pathList.push(route.path)
+      //   if (route.children) {
+      //     route.children.forEach(child => {
+      //       pathList.push(route.path + '/' + child.path)
+      //       console.log(route.path + '/' + child.path)
+      //     })
+      //   }
+      // })
+      // console.log(pathList.filter(p => {
+      //   return to.path === p
+      // }))
       if (store.getters.roles.length === 0) { // 判断当前用户是否已拉取完user_info信息
         store.dispatch('GetUserInfo').then(res => { // 拉取user_info
           // const roles = res.data.roles // note: roles must be a array! such as: ['editor','develop']
